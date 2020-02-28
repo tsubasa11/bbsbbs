6de4d3da1b713ab36b4d1e858ad3724850253712
tsubasa <tsubasa.namiki@rakus-partners.co.jp>
2020-02-28 11:53:01 +0900

    firstcommit

---
+++ src/main/java/com/example/BbsbbsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsbbsApplication.class, args);
+	}
+
+}
